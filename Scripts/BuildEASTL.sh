f42cf1b063f7cbf7709c3c4615e7861de8ac2b86
jp <jpgitpublic@gmail.com>
2024-08-03 00:10:59 +1000

    Added EASTL as a submodule along with a build script. Project magically links against the target library....? ¯\_(ツ)_/¯

---
+++ Scripts/BuildEASTL.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+EASTL_FOLDER=external/EASTL
+BUILD_FOLDER=build
+RELEASE_DIR=${BUILD_FOLDER}/release
+DEBUG_DIR=${BUILD_FOLDER}/debug
+
+pushd ${EASTL_FOLDER}
+
+echo "Clearing build folder ${EASTL_FOLDER}/${BUILD_FOLDER}"
+rm -rf ${BUILD_FOLDER}
+
+mkdir -p ${DEBUG_DIR}
+mkdir -p ${RELEASE_DIR}
+
+cmake . -DEASTL_BUILD_TESTS:BOOL=OFF -DEASTL_BUILD_BENCHMARK:BOOL=OFF
+cmake -S . -B ${RELEASE_DIR}
+echo "Building release"
+cmake --build ${RELEASE_DIR} --config Release -- -j 32 
+cmake -S . -B ${DEBUG_DIR}
+echo "Building debug"
+cmake --build ${DEBUG_DIR} --config Debug -- -j 32
+
+popd
---
+++ StringLiteral/StringView/StringView.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <EASTL/string.h>
+
+constexpr eastl::string_view LOCALE_FR_HELLO = "Bonjour %.*s! Comment allez-vous?\n";
+constexpr eastl::string_view LOCALE_EN_HELLO = "Hello %.*s! How are you?\n";
+
+constexpr const char* NAME_ELEANOR = "Eleanor Rigby";
+constexpr eastl::string_view NAME_TOM = "Tom";
+
+void SayHello(const eastl::string_view localised, const eastl::string_view fullName)
+{
+    size_t length = fullName.length();
+    if (length <= 0)
+    {
+        return;
+    }
+
+    size_t delimiterPosition = fullName.find(' ');
+    int firstNameLength = delimiterPosition != eastl::string_view::npos ? delimiterPosition : length;
+
+    printf(localised.data(), firstNameLength, fullName);
+}
+
+int main()
+{
+    SayHello(LOCALE_EN_HELLO, NAME_ELEANOR);
+    SayHello(LOCALE_FR_HELLO, NAME_TOM);
+
+    return 0;
+}
